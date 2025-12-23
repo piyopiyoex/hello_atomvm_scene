6987a1c769f52c4e20a13cd0f5f15b592415e67e
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2025-12-23 09:28:11 +0900

    feat: start input server only when required

--- lib/sample_app.ex
+++ lib/sample_app.ex
@@ -13,22 +13,35 @@ defmodule SampleApp do
         @display_port_options ++ [spi_host: spi_host]
       )
 
-    {:ok, touch_pid} =
-      SampleApp.TouchInput.start_link(
-        spi: spi_host,
-        device: :spi_dev_touch,
-        width: Keyword.get(@display_port_options, :width, 320),
-        height: Keyword.get(@display_port_options, :height, 240),
-        rotation: Keyword.get(@display_port_options, :rotation, 0),
-        touch: @touch_options
-      )
+    input_server_pid = maybe_start_input_server(spi_host)
 
     {:ok, _scene_pid} =
       @scene.start_link([],
         display_server: {:port, display_port},
-        input_server: touch_pid
+        input_server: input_server_pid
       )
 
     Process.sleep(:infinity)
   end
+
+  defp maybe_start_input_server(spi_host) do
+    if input_server_required?(@scene) do
+      {:ok, pid} =
+        SampleApp.TouchInput.start_link(
+          spi: spi_host,
+          device: :spi_dev_touch,
+          width: Keyword.fetch!(@display_port_options, :width),
+          height: Keyword.fetch!(@display_port_options, :height),
+          rotation: Keyword.fetch!(@display_port_options, :rotation),
+          touch: @touch_options
+        )
+
+      pid
+    else
+      nil
+    end
+  end
+
+  defp input_server_required?(SampleApp.TouchCalibrationScene), do: true
+  defp input_server_required?(_), do: false
 end
